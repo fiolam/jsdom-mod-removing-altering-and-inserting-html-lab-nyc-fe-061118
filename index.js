70d5f7679c7b532774abddc4c3fb869c0f3136f1
fiolam <fiola.mulliqi@wework.com>
2018-07-12 17:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 /* Enter the code to remove the main node element under this comment */
 
+<<<<<<< HEAD
 var main = document.getElementById('main')
 main.remove();
+=======
+
+>>>>>>> a11fbc62d02d92b105b12fe0be2eabc7ac8c8a9d
 
 /* Create your new element here and assign it to newHeader */
 
 const newHeader = document.createElement('h1');
+<<<<<<< HEAD
 newHeader.innerText = "Fiola is the champion!";
+=======
+// next: set some text on newHeader
+ newHeader.innerHTML = "Fiola is the champion!"
+>>>>>>> a11fbc62d02d92b105b12fe0be2eabc7ac8c8a9d
 newHeader.setAttribute("id", "victory")
 // next: find the body element
 var body = document.querySelector('body')
